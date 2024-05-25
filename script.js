df5ba5338ecb6382a4baad3ef2523582dc0389e3
salam46khan <salam46.khan@gmail.com>
2024-05-25 16:52:25 +0600

    finished

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ console.log('hii');
 
 const navList = document.getElementById('nav-list')
 const loginBtn = document.getElementById('login-btns')
-navList.style.maxHeight = "0px"
-loginBtn.style.maxHeight = "0px"
+navList.style.top = "100px"
+loginBtn.style.top = "100px"
 
 function toggleMenu(){
-    if(navList.style.maxHeight == "0px"){
-        navList.style.maxHeight = "300px"
+    if(navList.style.top == "100px"){
+        navList.style.top = "-300px"
     }
     else{
-        navList.style.maxHeight = "0px"
+        navList.style.top = "100px"
     }
 }
 function toggleLogin(){
-    if(loginBtn.style.maxHeight == "0px"){
-        loginBtn.style.maxHeight = "200px"
+    if(loginBtn.style.top == "100px"){
+        loginBtn.style.top = "-200px"
     }
     else{
-        loginBtn.style.maxHeight = "0px"
+        loginBtn.style.top = "100px"
     }
+}
+
+const faqBox = document.getElementsByClassName("faq-box")
+for(i = 0; i<faqBox.length; i++){
+    faqBox[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
 }
